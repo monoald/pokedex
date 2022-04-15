ab1b954816c637a33280c2cc60fe197c9679b4dc
Carlos Aldazosa <carlos.aldazosa@outlook.com>
2022-04-14 21:05:44 -0400

    Show the corresponding pokemons according to the active region

--- pokedex/js/index.js
+++ pokedex/js/index.js
@@ -47,7 +47,7 @@ const backIcon = document.getElementById('back-icon');
 backIcon.addEventListener('click', hideFilters);
 
 
-//----------------------REQUEST------------------------
+//------------------TYPES REQUEST------------------------
 
 
 //  Get type container to append new types on it
@@ -55,17 +55,16 @@ const typesContainer = document.getElementById('type-container');
 
 const getTypes = async (url_api) => {
 	try {
-
+		
 		const types = await fetchData(`${url_api}type/`);
-
+		
 		//  Append types for filters
 		types.results.map( type => {
 			const newType = makeType(type.name);
 			typesContainer.appendChild(newType)
 		} );
-
 	} catch (error) {
-			console.error(error);
+		console.error(error);
 	}
 };
 getTypes(API);
@@ -75,21 +74,29 @@ const pushElement = (array, pokemons) => {
 	array.map( pokemon => {
 		pokemons.push(pokemon)
 	})
-}
+};
 
-const actualRegion = ['national', '1'];
+//---------------------REGION ACTIVE----------------------
 
-//	Container to append cards
-const cardsContainer = document.getElementById('cards-container');
 
-const getPokemonInfo = async (url_api) => {
+//	Get the active region
+const regionActive = document.getElementsByClassName('region__item--active');
+//	Store the active region
+const region = (regionActive[0].innerText).toLowerCase();
+
+
+//--------------POKEMON REQUEST BUILD CARDS--------------
+
+let cardsContainer = document.getElementById('cards-container');
+const getPokemonInfo = async (url_api, actualRegion, htmlElement) => {
 	try {
 		
+		//	Container to append cards
 		const regions = [];
 
 		//	Get all regions
-		const regionsPage1 = await fetchData(`${url_api}pokedex/`)
-		const regionsPage2 = await fetchData(`${regionsPage1.next}`)
+		const regionsPage1 = await fetchData(`${url_api}pokedex/`);
+		const regionsPage2 = await fetchData(`${regionsPage1.next}`);
 		
 		//	Store all regions
 		regionsPage1.results.map( region => {
@@ -100,9 +107,14 @@ const getPokemonInfo = async (url_api) => {
 			regions.push(region);
 		})
 
-		//	Get region pokedex
-		const regionPokedex = await fetchData(regions[0].url);
-		
+		//	Get the actual region object
+		let regionPokedex;
+		for (const element of regions) {
+			if (element.name == actualRegion) {
+				regionPokedex = await fetchData(element.url);
+			}
+		}
+
 		//	Make a card for each Pokemon
 		for (const element of regionPokedex.pokemon_entries) {
 			const pokemon = await fetchData(element.pokemon_species.url);
@@ -110,7 +122,7 @@ const getPokemonInfo = async (url_api) => {
 			
 			//	Make cards
 			const newPokemon = makeCard(pokemonInfo);
-			cardsContainer.appendChild(newPokemon);
+			htmlElement.appendChild(newPokemon);
 
 			//	Make pokemon type and append to the card
 			const cardTypes = document.getElementById(`card__${pokemonInfo.name}`);
@@ -123,4 +135,47 @@ const getPokemonInfo = async (url_api) => {
 	}
 }
 
-getPokemonInfo(API);
+//	Show initial pokedex
+getPokemonInfo(API, region, cardsContainer);
+
+
+//------------------------SWITCH POKEDEX-------------------
+
+
+//	Get the main element to delete the card element
+const main = document.getElementById('main');
+	
+//	Get the region elements
+const regionsElement = document.querySelectorAll('.region__item');
+//	Hear every element to action the event
+regionsElement.forEach( element => {
+	element.addEventListener('click', switchRegions);
+});
+
+//	Function to build the pokedex according to the actual region
+function switchRegions() {
+	//	Remove element to stop previous request and clear cards
+	cardsContainer.remove();
+
+	//	Re-Creating the card container element for the new pokedex
+	cardsContainer = document.createElement('section');
+	cardsContainer.classList.add('main__cards');
+	cardsContainer.setAttribute('id', 'cards-container');
+	main.appendChild(cardsContainer);
+
+	//	Deactivate every region element
+	regionsElement.forEach( region => {
+		region.classList.remove('region__item--active');
+	});
+	
+	//	Activate the selected region
+	this.classList.add('region__item--active');
+	
+	//	Get the new region name
+	const regionActive = document.getElementsByClassName('region__item--active');
+	const region = (regionActive[0].innerText).toLowerCase();
+
+	//	Build pokedex according to the active region
+	getPokemonInfo(API, region, cardsContainer);
+};
+
